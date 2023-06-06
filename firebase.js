6f5da6322658d33e78ac7c568772c4aa2ca41a2a
bjames236 <gicanabobby@gmail.com>
2023-06-06 13:10:40 +0800

    Login and Register

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLA4vnLFaVow25ylLUkb57NhmnN6qk1vE",
+  authDomain: "fir-auth-65294.firebaseapp.com",
+  projectId: "fir-auth-65294",
+  storageBucket: "fir-auth-65294.appspot.com",
+  messagingSenderId: "105428798939",
+  appId: "1:105428798939:web:49a9375f32ea94d4dfebfe"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+const auth = firebase.auth()
+
+export { auth };
